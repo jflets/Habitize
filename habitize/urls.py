319595f0ddef9aff05d4340e1cee903040bc3994
jflets <jfletorides02@gmail.com>
2023-09-19 13:04:33 +0100

    Style login and sign up page

--- habitize/urls.py
+++ habitize/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("allauth.urls")),
     path('', include('landingpage.urls'), name='landingpage_urls'),
+    path("accounts/", include("allauth.urls")),
 ]
