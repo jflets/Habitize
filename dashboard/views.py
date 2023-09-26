432a93c2869d45119badbb8df866ad5201102b8c
jflets <jfletorides02@gmail.com>
2023-09-26 09:46:14 +0100

    Add habit frequency filter and frequency progress

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,7 +6,22 @@ from .forms import AddHabitForm, EditHabitForm
 
 @login_required(login_url="/accounts/login")
 def get_dashboard(request):
+    # Get the selected frequency from the request
+    selected_frequency = request.GET.get('frequency')
+
+    # Filter habits based on the selected frequency
     habits = Habit.objects.filter(user=request.user)
+
+    if selected_frequency == 'day':
+        habits = habits.filter(frequency='day')
+    elif selected_frequency == 'week':
+        habits = habits.filter(frequency='week')
+    elif selected_frequency == 'month':
+        habits = habits.filter(frequency='month')
+    else:
+        # Handle cases where an invalid frequency is selected
+        habits = habits.all()  # Display all habits, you can customize this behavior
+
     total_habits = habits.count()
     completed_habits = habits.filter(completed=True).count()
 
@@ -24,11 +39,41 @@ def get_dashboard(request):
     total_completed_progress = (
         completed_habits / total_habits) * 100 if total_habits > 0 else 0
 
+    # Calculate progress for completed habits by frequency
+    completed_habits_day = habits.filter(
+        completed=True, frequency='day').count()
+    completed_habits_week = habits.filter(
+        completed=True, frequency='week').count()
+    completed_habits_month = habits.filter(
+        completed=True, frequency='month').count()
+
+    # Calculate the ratio of completed habits for each frequency
+    total_habits_day = habits.filter(frequency='day').count()
+    total_habits_week = habits.filter(frequency='week').count()
+    total_habits_month = habits.filter(frequency='month').count()
+
+    completed_progress_day = (
+        completed_habits_day / total_habits_day) * 100 if total_habits_day > 0 else 0
+    completed_progress_week = (
+        completed_habits_week / total_habits_week) * 100 if total_habits_week > 0 else 0
+    completed_progress_month = (
+        completed_habits_month / total_habits_month * 100) if total_habits_month > 0 else 0
+
     context = {
         'habits': habits,
         'total_habits': total_habits,
         'completed_habits': completed_habits,
         'total_completed_progress': total_completed_progress,
+        'selected_frequency': selected_frequency,
+        'completed_habits_day': completed_habits_day,
+        'completed_habits_week': completed_habits_week,
+        'completed_habits_month': completed_habits_month,
+        'completed_progress_day': completed_progress_day,
+        'completed_progress_week': completed_progress_week,
+        'completed_progress_month': completed_progress_month,
+        'total_habits_day': total_habits_day,
+        'total_habits_week': total_habits_week,
+        'total_habits_month': total_habits_month,
     }
     return render(request, 'dashboard/index.html', context)
 
