b0958bb314416002a502e1c8fd0099edeb785a2e
jflets <jfletorides02@gmail.com>
2023-09-26 16:10:10 +0100

    Adjust user_profile model to include cloudinary

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# Define choices for color themes
+COLOR_CHOICES = [
+    ('light', 'Light Theme'),
+    ('dark', 'Dark Theme'),
+    # Add more choices as needed
+]
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    color_theme = models.CharField(
+        max_length=20, choices=COLOR_CHOICES, default='light')
+    profile_image = CloudinaryField('image', null=True, blank=True)
+    profile_image_public_id = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
