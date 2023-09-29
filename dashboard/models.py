07bf894964a4527279319fec0466d65b35c9cec4
jflets <jfletorides02@gmail.com>
2023-09-29 14:11:57 +0100

    Add habit reset to habit model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Habit(models.Model):
@@ -10,7 +11,6 @@ class Habit(models.Model):
     value = models.PositiveIntegerField(default=0)  # Current value
     goal_amount = models.PositiveIntegerField(default=100)  # Maximum value
 
-    # New fields
     frequency = models.CharField(
         max_length=10,
         choices=[
@@ -22,6 +22,8 @@ class Habit(models.Model):
         null=False,
         blank=False
     )
+
+    last_reset = models.DateTimeField(null=True, blank=True)
 
     units = models.CharField(
         max_length=50,
@@ -36,6 +38,21 @@ class Habit(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def reset_habit(self):
+        # Check if the habit's frequency is set to 'daily'
+        if self.frequency == 'day':
+            # Check if the habit hasn't been reset today
+            if self.last_reset is None or (timezone.now() - self.last_reset).days >= 1:
+                # Reset the habit by updating its value and last reset time
+                self.value = 0
+                self.last_reset = timezone.now()
+                self.save()
+
+    def save(self, *args, **kwargs):
+        # Call the reset_habit method before saving the habit
+        self.reset_habit()
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['completed']
