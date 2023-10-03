24f1f5f7f3376b49106e72b4e3bf812a772a1212
jflets <jfletorides02@gmail.com>
2023-10-03 14:32:00 +0100

    Add tests for all apps

--- landingpage/tests.py
+++ landingpage/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class LandingPageViewTestCase(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        # Check if the URL for the landingPage view exists and returns a 200 status code.
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        # Check if the landingPage view can be accessed by its name.
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        # Check if the landingPage view uses the correct template.
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'landing_page/index.html')
